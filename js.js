216353ac5d81f13974b35e048640acaee54cb52f
Marjon07 <126189100+Marjon07@users.noreply.github.com>
2023-02-24 00:37:47 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,26 +1,22 @@
-// Select the form element
 const form = document.querySelector('form');
 
-// Add event listener for form submission
-form.addEventListener('submit', function(e) {
-  e.preventDefault(); // Prevent the form from submitting normally
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // Prevent the default form submission
 
-  // Get form field values
-  const name = form.querySelector('input[name="name"]').value;
-  const surname = form.querySelector('input[name="surname"]').value;
-  const birthday = form.querySelector('input[name="birthday"]').value;
-  const gender = form.querySelector('input[name="gender"]:checked').value;
-  const location = form.querySelector('select[name="location"]').value;
-  const buildingType = form.querySelector('select[name="building-type"]').value;
-  const budget = form.querySelector('input[name="budget"]:checked').value;
+  // Get the form input values
+  const name = form.querySelector('#name').value;
+  const email = form.querySelector('#email').value;
+  const buildingType = form.querySelector('#building-type').value;
+  const budget = form.querySelector('#budget').value;
+  const message = form.querySelector('#message').value;
 
-  // Send form data to email address
-  const email = 'pulsbuilds@gmail.com';
-  const subject = 'New building project inquiry';
-  const body = 'Name: ' + name + '\n' + 'Surname: ' + surname + '\n' + 'Birthday: ' + birthday + '\n' + 'Gender: ' + gender + '\n' + 'Location: ' + location + '\n' + 'Building type: ' + buildingType + '\n' + 'Budget: ' + budget;
-  window.location.href = 'mailto:' + email + '?subject=' + subject + '&body=' + encodeURIComponent(body);
+  // Compose the email body
+  const subject = `New Project Inquiry: ${buildingType}`;
+  const body = `Name: ${name}\nEmail: ${email}\nBuilding Type: ${buildingType}\nBudget: ${budget}\nMessage: ${message}`;
 
-  // Display confirmation message
-  const confirmationMsg = document.getElementById('confirmation-msg');
-  confirmationMsg.style.display = 'block';
+  // Compose the mailto link with the email body
+  const mailtoLink = `mailto:pulsbuilds@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+  // Open the default email client with the mailto link
+  window.location.href = mailtoLink;
 });
